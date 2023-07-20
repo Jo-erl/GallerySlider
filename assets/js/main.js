7ce8755e2a9eee5905e5ad00c200c7176f89cd9d
Joel Anang <133300552+Jo-erl@users.noreply.github.com>
2023-07-20 00:04:31 +0000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,18 @@
-/*=============== SWIPER JS GALLERY ===============*/
 let swiperCards = new Swiper(".gallery-cards", {
   loop: true,
   loopedSlides: 5,
   cssMode: true,
   effect: 'fade',
+  autoHeight: true, // Adjust the container height based on the active slide's height
 });
-  
+
 let swiperThumbs = new Swiper(".gallery-thumbs", {
   loop: true,
   loopedSlides: 5,
   slidesPerView: 3,
   centeredSlides: true,
   slideToClickedSlide: true,
+  autoHeight: true, // Adjust the container height based on the active slide's height
 
   pagination: {
     el: ".swiper-pagination",
